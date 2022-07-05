3bb92a169aae06a4a3508560ce6271cb6ca07e67
syedaasik <syedaasik.cs@gmail.com>
2022-07-05 19:37:33 +0530

    new test program

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for i in `seq 1 100`
-do
-    echo "$i times";
-done
+file=$1;
+ls $file > /dev/null 2> /dev/null;
+result=$(echo $?);
+if [ $result -eq 0 ];
+    then
+    echo "$1 file exists";
+else
+    echo "$1 file not exists";
+fi 
